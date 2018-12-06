cab07051cba6bf7bb5b6eba7f5b5af3a1894d460
Alve Ulander <alve.ulander@xtractor.se>
2018-12-06 15:56:03 +0100

    initial commit

---
+++ src.js
@@ -0,0 +1,22 @@
+class Hexagon {
+    constructor() {
+        this.top = '<div class="hexagon-top"></div>'
+        this.center = '<div class="hexagon-center"></div>'
+        this.bottom = '<div class="hexagon-bottom"></div>'
+    }
+}
+
+renderHexes = (hexContainer) => {
+    hexesToRender = 1
+    for (let i = 0; i < hexesToRender; i++) {
+        const hexagon = new Hexagon
+        hexContainer.insertAdjacentHTML('beforeend', hexagon.top)
+        hexContainer.insertAdjacentHTML('beforeend', hexagon.center)
+        hexContainer.insertAdjacentHTML('beforeend', hexagon.bottom)
+
+    }
+}
+
+window.onload = () => {
+    renderHexes(document.getElementById('hex-container'))
+}
