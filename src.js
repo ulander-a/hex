6506cb7b1e09b3f72296c6cac143166e1cb82e91
ulander-a <alve.ulander@chasacademy.se>
2018-12-06 19:43:10 +0100

    add some scaffolding and pseudocode

--- src.js
+++ src.js
@@ -1,21 +1,54 @@
 class Hexagon {
     constructor() {
-        this.top = '<div class="hexagon-top"></div>'
-        this.center = '<div class="hexagon-center"></div>'
-        this.bottom = '<div class="hexagon-bottom"></div>'
+        this.left = '<div class="left"></div>'
+        this.center = '<div class="center"></div>'
+        this.right = '<div class="right"></div>'
     }
 }
 
-renderHexes = (hexContainer) => {
-    hexesToRender = 1
-    for (let i = 0; i < hexesToRender; i++) {
-        const hexagon = new Hexagon
-        hexContainer.insertAdjacentHTML('beforeend', hexagon.top)
-        hexContainer.insertAdjacentHTML('beforeend', hexagon.center)
-        hexContainer.insertAdjacentHTML('beforeend', hexagon.bottom)
+const createHex = () => {
+    
+}
+
+const createRows = length => {
+
+}
+
+const calcRows = amnt => {
+    amnt.length > 4 
+}
+
+const renderHexes = (container, amnt) => {
+    // first row up to 4
+    // second row
+    // if second row == first row +1 create another row
+    // repeat until latest row +1 != previous row, then just render remaining hexes
+
+    if (amnt.length > 4) {
+        // create new row 
+        // if amnt.length > new row.length - create new row
+        // if amnt.length > new row.length - create new row 
+    } else {
+        // just create a row of what we have
     }
+
+    // render rows
+
+    return amnt.map(() => {
+        const hexagon = new Hexagon
+        container.insertAdjacentHTML(
+            'beforeend',
+            `<div class="hex">
+                ${hexagon.left}
+                ${hexagon.center}
+                ${hexagon.right}
+            </div>`
+        )
+    })
 }
 
 window.onload = () => {
-    renderHexes(document.getElementById('hex-container'))
+    const container = document.getElementById('hex-container')
+    console.log(container)
+    renderHexes(container, [...Array(4)])
 }
