5b23e0acd80530bcedcc2a624a24edde9a9bc3cb
Alve Ulander <alve.ulander@xtractor.se>
2018-12-10 16:39:40 +0100

    js wip

--- javascript/src.js
+++ javascript/src.js
@@ -1,36 +1,37 @@
 const rowHTML = length => {
-    const test = [...Array(length)].map((value, index) => {
+    const test = [...Array(length)].map((v, i) => {
         return `
-        <div class="${index % 2 == 0 ? 'hex even' : 'hex'}">
+        <div class="${i % 2 == 0 ? 'hex even' : 'hex'}">
             <div class="left"></div>
             <div class="center"></div>
             <div class="right"></div>
         </div>`
     })
-    debugger
     return test.join('')
 }
 
 const calcRows = amnt => {
-    let rowLength = 3
-
-    const rows = [...Array(amnt)].map(() => {
-        // create rows with varying length from top to bottom
-        // rowLenght = 3
-        // while rowLength < amnt create new row and return new rowLength
-        while (rowLength < amnt) {
-            const row = `<div class="hex-row">${rowHTML(rowLength)}</div>`
-            rowLength++
-
-            return row
-        }
+    const arr = [...Array(amnt)].map((v, i) => {
+        return i
     })
 
-    return rows
+
+    // const grid = arr.concat(arr)
+    // console.log(grid) 
+
+    // const rows = [...Array(amnt)].map(() => {
+    //     while (rowLength < amnt) {
+    //         const row = `<div class="hex-row">${rowHTML(rowLength)}</div>`
+    //         rowLength++
+    //         return row
+    //     }
+    // })
+
+    // return rows
 }
 
-const render = (arr, container) => {
-    return arr.map(element => {
+const render = (array, container) => {
+    return array.map(element => {
         container.insertAdjacentHTML(
             'beforeend',
             element
@@ -40,6 +41,6 @@ const render = (arr, container) => {
 
 window.onload = () => {
     const container = document.getElementById('hex-container')
-    const rows = calcRows(8)
-    render(rows, container)
+    const rows = calcRows(5)
+    // render(rows, container)
 }
