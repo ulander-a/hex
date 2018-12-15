d814c22eb8d7e8304c06bd5ee30e97ae360ac28e
ulander-a <alve.ulander@chasacademy.se>
2018-12-15 01:50:36 +0100

    refactor

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -1,5 +1,5 @@
-const calcHexCornerCoord = (center, size, i) => {
-    const angleDeg = 60  * i - 30
+const calcHex = (center, size, i) => {
+    const angleDeg = 60 * i - 30
     const angleRad = Math.PI / 180 * angleDeg
     const x = center.x + size * Math.cos(angleRad)
     const y = center.y + size * Math.sin(angleRad)
@@ -10,16 +10,20 @@ const point = (x, y) => {
     return { x: x, y: y }
 }
 
-const drawHex = (canvasID, center) => {
-    for(let i = 0; i <= 5; i++) {
-        let start = calcHexCornerCoord(center, 20, i)
-        let end = calcHexCornerCoord(center, 20, i+1)
-        drawLine(canvasID, {x: start.x, y: start.y}, {x: end.x, y: end.y})
-    }
+const drawHex = (canvas, center) => {
+    return [...Array(6)].map((v, i) => {
+        const start = calcHex(center, 20, i)
+        const end = calcHex(center, 20, i + 1)
+        drawLine(
+            canvas,
+            { x: start.x, y: start.y },
+            { x: end.x, y: end.y }
+        )
+    })
 }
 
-const drawLine = (canvasID, start, end) => {
-    const ctx = canvasID.getContext('2d')
+const drawLine = (canvas, start, end) => {
+    const ctx = canvas.getContext('2d')
     ctx.beginPath()
     ctx.moveTo(start.x, start.y)
     ctx.lineTo(end.x, end.y)
@@ -28,6 +32,6 @@ const drawLine = (canvasID, start, end) => {
 }
 
 window.onload = () => {
-    const canvasID = document.getElementById('hex-container')
-    drawHex(canvasID, {x: 50, y: 50})
+    const canvas = document.getElementById('hex-container')
+    return drawHex(canvas, { x: 50, y: 50 })
 }
