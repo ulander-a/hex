0ea36bddf8bd7d6715872a7ad0d2c82211a371f0
ulander-a <alve.ulander@chasacademy.se>
2018-12-15 02:23:30 +0100

    add live drawing

--- javascript/canvas.js
+++ javascript/canvas.js
@@ -6,8 +6,19 @@ const calcHex = (center, size, i) => {
     return point(x, y)
 }
 
+const hexToPixel = (hex, size = 20) => {
+    const hexOrigin = {x: 300, y: 300}
+    const x = size * (Math.sqrt(3) * hex.c + Math.sqrt(3) / 2 * hex.r) + hexOrigin.x
+    const y = size * (3. / 2 * hex.r) + hexOrigin.y
+    return point(x, y)
+}
+
 const point = (x, y) => {
     return { x: x, y: y }
+}
+
+const hex = (r, c) => {
+    return { r: r, c: c }
 }
 
 const drawHex = (canvas, center) => {
@@ -31,7 +42,24 @@ const drawLine = (canvas, start, end) => {
     ctx.closePath()
 }
 
+const drawHexes = canvas => {
+    for (let r = 0; r <= 4; r++) {
+        for (let c = 0; c <= 4; c++) {
+            let center = hexToPixel(hex(r, c))
+            drawHex(canvas, center)
+            drawCoordinates(canvas, center, hex(c, r))
+        }
+    }
+}
+
+const drawCoordinates = (canvas, center, hex) => {
+    const ctx = canvas.getContext('2d')
+    ctx.fillText(hex.c, center.x-10, center.y)
+    ctx.fillText(hex.r, center.x+5, center.y)
+}
+
 window.onload = () => {
     const canvas = document.getElementById('hex-container')
-    return drawHex(canvas, { x: 50, y: 50 })
+    // return drawHex(canvas, { x: 50, y: 50 })
+    return drawHexes(canvas)
 }
