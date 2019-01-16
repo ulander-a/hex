d06f84619fb6f317982d76ba26c255a06daf6185
Alve Ulander <alve.ulander@xtractor.se>
2019-01-16 12:01:05 +0100

    add redux + extras

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+
+// redux stuff
+import { Provider } from 'react-redux'
+import configureStore from './configureStore'
+
+// Move this later (turn App to a component etc)
 import { Header, Grid, Options, Info } from './components'
+import './App.css'
+
+const store = configureStore()
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <main>
-          <Options />
-          <Grid />
-          <Info />
-        </main>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Header />
+          <main>
+            <Options />
+            <Grid />
+            <Info />
+          </main>
+        </div>
+      </Provider>
     );
   }
 }
