ccf47c2f548fa4265ad8794ca84287beff6c7ebc
Alve Ulander <alve.ulander@xtractor.se>
2019-01-25 14:03:38 +0100

    begin refactor

---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+// import { CHANGE_OPTION, CREATE_GRID } from '../components/Options/actions'
+
+// const defaultState = {
+//   test: 'tjofaderittan hambo!',
+//   option: 'unchanged',
+//   articles: [],
+// }
+
+// const rootReducer = (state = defaultState, action) => {
+//   switch (action.type) {
+//     case CHANGE_OPTION:
+//       return {
+//         ...state,
+//         option: 'changed'
+//       }
+//     default:
+//       return state
+//   }
+// }
+
+// export default rootReducer
