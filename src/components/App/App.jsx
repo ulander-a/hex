33208d15567177069556629c25c9ad1efefcadd2
Alve Ulander <alve.ulander@xtractor.se>
2019-01-25 16:11:00 +0100

    redux sorta works

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ const App = () => (
     <div className="App">
         <Header />
         <main>
-            {/* <Options /> */}
+            <Options />
             <Grid />
             <Info />
         </main>
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import OptionsForm from '../OptionsForm/OptionsForm'
 import './Options.css'
+import {connect} from 'react-redux'
+const mapStateToProps = state => {
+  return { option: state.option || 'Sneed\'s seed & feed (formerly Chuck\'s)'};
+};
+const Options = ({ option }) => (
+  <aside className="options">
+    <h2>Options:</h2>
+    <p>{option}</p>
+    {/* <OptionsForm /> */}
+  </aside>
+)
 
-export default function Options() {
-  return (
-    <aside className="options">
-      <h2>Options:</h2>
-      <OptionsForm />
-    </aside>
-  )
-}
+
+
+export default connect(mapStateToProps)(Options)
