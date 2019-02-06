12d0ab0d01bee2f5dffa2e4db6cda49b1129f3c5
Alve Ulander <alve.ulander@xtractor.se>
2019-02-06 13:24:48 +0100

    dotenv

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 5050
+
+if (!process.env.PORT) { require('dotenv').config() }
+const port = process.env.PORT
+
 
 app.get('/', (req, res) =>{
   res.send('Hello world!')
 })
 
 app.listen(port, () => {
-  console.log(`[Server up] Go to: http://localhost:${port}/ `)
+  console.log(`[Server up] Go to: http://localhost:${process.env.PORT}/ `)
 })
