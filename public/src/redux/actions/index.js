b0b578e2a9140b1c2e2b62c4ccf47d04e0d45b35
Alve Ulander <alve.ulander@xtractor.se>
2019-02-07 15:38:45 +0100

    begin implemeting api actions

--- public/src/redux/actions/index.js
+++ public/src/redux/actions/index.js
@@ -1,9 +1,19 @@
 import {
     SET_OPTIONS,
+    CREATE_GRID,
     HIGHLIGHT_HEX,
     ADD_DATA_TO_HEX,
-    CREATE_GRID,
+    SAVE_GRID_START,
+    SAVE_GRID_SUCCESS,
+    SAVE_GRID_FAILURE,
+    GET_GRID_START,
+    GET_GRID_SUCCESS,
+    GET_GRID_FAILURE,
 } from '../constants/action-types'
+
+const API = process.env.API_URL
+
+// Client-side actions
 
 export const setOptions = options => ({
     type: SET_OPTIONS,
@@ -24,3 +34,49 @@ export const addDataToHex = hex => ({
     type: ADD_DATA_TO_HEX,
     payload: hex
 })
+
+/**
+ * Server-side actions 
+ */
+// CREATE grid
+
+// GET grid
+export const getGridStart = () => ({type: GET_GRID_START}) 
+export const getGridSuccess = grid => ({
+    type: GET_GRID_SUCCESS,
+    payload: grid
+})
+export const getGridFailure = error => ({
+    type: GET_GRID_FAILURE,
+    payload: error
+})
+
+export const getGrid = id => dispatch => {
+    dispatch(getGridStart)
+
+    return fetch(`${API}/grids/${id}`, {
+        method: 'GET'
+    }).then(grid => getGridSuccess(grid))
+    .catch(error => getGridFailure(error))
+}
+
+ // SAVE grid
+export const saveGridStart = () => ({type: SAVE_GRID_START})
+export const saveGridSuccess = res => ({
+    type: SAVE_GRID_SUCCESS,
+    payload: res
+})
+export const saveGridFailure = error => ({
+    type: SAVE_GRID_FAILURE,
+    payload: error
+})
+
+export const saveGrid = grid => dispatch => {
+    dispatch(saveGridStart)
+
+    return fetch(`${API}/grids`, {
+        method: 'POST',
+        body: grid,
+    }).then(res => saveGridSuccess(res))
+    .catch(error => saveGridFailure(error))
+}
