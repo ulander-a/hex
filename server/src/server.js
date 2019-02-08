9faa57e1237480248f079f07f7c1bae22aa12900
Alve Ulander <alve.ulander@xtractor.se>
2019-02-08 12:42:40 +0100

    enable cors

--- server/src/server.js
+++ server/src/server.js
@@ -8,6 +8,14 @@ const db = require('./db')
 const TestController = require('./controllers/TestController')
 const GridController = require('./controllers/GridController')
 
+// Enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 // Routes
 app.get('/', (req, res) =>{
   res.send('Hello world!')
