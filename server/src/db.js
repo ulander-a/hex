bb1b03fde8d51e7bdfdff3751be10efd4c627c68
Alve Ulander <alve.ulander@xtractor.se>
2019-02-20 15:01:46 +0100

    update to use chalk

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const chalk = require('chalk')
 mongoose.connect(
     `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@ds117334.mlab.com:17334/hex`,
     {useNewUrlParser: true},
     err => {
         if (err) {
-            console.log('There was a problem connecting to MLAB: ', err)
+            console.log(chalk.red('There was a problem connecting to MLAB: ', err))
         } else {
-            console.log('Connection to MLAB was successful')
+            console.log(chalk.green('Connection to MLAB was successful'))
         }
     })
